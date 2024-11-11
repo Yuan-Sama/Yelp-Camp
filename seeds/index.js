b65dfb27af71f10ff35c5292a56a63b268c0e661
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-11 11:27:13 +0700

    update seed

--- seeds/index.js
+++ seeds/index.js
@@ -18,10 +18,15 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
 			// @ts-ignore
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
-			title: `${sample(descriptors)} ${sample(places)}`
+			title: `${sample(descriptors)} ${sample(places)}`,
+			image: `https://picsum.photos/400?random=${Math.random()}`,
+			description:
+				'Nulla sagittis ipsum condimentum, elementum velit dapibus, pulvinar nisl. Aenean id metus a arcu semper molestie. In felis libero, cursus quis ante sit amet, blandit mollis sem. Vivamus dui nunc, accumsan eget mattis ut, molestie quis erat. Fusce finibus justo suscipit, pharetra nunc sed, pellentesque ligula. Pellentesque suscipit sem nec ultrices imperdiet. Cras semper tellus a nisi luctus, in ornare lorem aliquam. Etiam facilisis dui arcu, sit amet dapibus quam porta quis. Phasellus ut facilisis enim. Pellentesque vel lectus in libero bibendum dapibus id ut ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at odio lacus.',
+			price
 		});
 		await camp.save();
 	}
